47419537ff2ef455eb329307f78c7947107bbab6
unknown <4070267@HS222-19.vtsd.com>
2015-09-25 11:50:02 -0400

    Lab 03Dst

---
+++ Lab03Dst.java
@@ -0,0 +1,38 @@
+// Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+
+
+public class Lab03Dst
+{
+    public static void main(String args[])
+    {
+        System.out.println("Lab 03D");
+        System.out.println();
+        int num1 = 11;
+        int num2 = 22;
+        int num3 = 33;
+        int num4 = 44;
+        int num5 = 55;
+
+        System.out.println();
+        System.out.println("Number 1 = "+num1+" Number 2 = "+num2+" Number 3 = "+num3+" Number 4 = "+num4+" Number 5 = "+num5);
+        System.out.println("adverage");
+        int result=(num1+num2+num3+num4+num5)/5;
+        System.out.println((num1+num2+num3+num4+num5) /5);
+        System.out.println();
+        System.out.println("100 Point Version");
+        double number1 = 15;
+        double number2 = 53;
+        double number3 = 63;
+        double number4 = 500;
+        double number5 = 42;
+        System.out.println("Number 1 = "+number1+" Number 2 = "+number2+" Number 3 = "+number3+" Number 4 = "+number4+" Number 5 = "+number5);
+        System.out.println("adverage");
+        double result2=(number1+number2+number3+number4+number5) /5;
+        System.out.println((number1+number2+number3+number4+number5) /5);
+
+
+        System.out.println();
+    }
+}
